3e2bbf41ab11bdd6958ec81422e150d331a732ae
dhruvalraval <imadhruvalraval@gmail.com>
2024-11-14 22:29:46 +0530

    init commit test

---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import {FlatCompat} from '@eslint/eslintrc'
+import js from '@eslint/js'
+import typeScriptParser from '@typescript-eslint/parser'
+import reactRefreshPlugin from 'eslint-plugin-react-refresh'
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort'
+
+// Initialize FlatCompat with recommendedConfig
+const compat = new FlatCompat({
+  recommendedConfig: js.configs.recommended,
+})
+
+export default [
+  // Direct usage of compat with specific configs
+  ...compat.extends('eslint:recommended'),
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  ...compat.extends('plugin:react-hooks/recommended'),
+
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typeScriptParser,
+      parserOptions: {
+        ecmaVersion: '2020',
+        sourceType: 'module',
+      },
+    },
+    ignores: ['dist', '.eslintrc.cjs', 'node_modules/'],
+    plugins: {
+      'react-refresh': reactRefreshPlugin,
+      'simple-import-sort': simpleImportSortPlugin,
+    },
+    rules: {
+      'react-refresh/only-export-components': ['warn', {allowConstantExport: true}],
+      quotes: ['error', 'single', {avoidEscape: true}],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-empty-function': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'error',
+      'prefer-const': 'error',
+      'prefer-destructuring': 'off',
+      'no-unused-expressions': 'off',
+      'no-duplicate-imports': 'error',
+      'prefer-template': 'error',
+      'no-var': 'error',
+      'object-shorthand': 'error',
+      'no-useless-escape': 'off',
+      'no-constant-condition': 'warn',
+      'no-extra-semi': 'off',
+      'react-hooks/exhaustive-deps': [
+        'warn',
+        {
+          additionalHooks: 'useDebouncedEffect',
+        },
+      ],
+      'no-unreachable': 'warn',
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [['^react', '^\\u0000', '^@?\\w', '@/(.*)', '^[./]']],
+        },
+      ],
+    },
+  },
+]
